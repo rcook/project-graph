16bf8765c591531658a32fd5b6e9c10ff0d9d5c4
Richard Cook <rcook@rcook.org>
2018-11-01 09:57:34 -0700

    Rename function

--- lib/ProjectGraph/TopSort.hs
+++ lib/ProjectGraph/TopSort.hs
@@ -18,8 +18,8 @@ flipMap :: (Ord k, Ord a) => Map k a -> Map a k
 flipMap = Map.fromList . map (\(k, a) -> (a, k)) . Map.toList
 
 leafIndices :: Ord k => [Dependency k] -> Map k Int
-leafIndices = foldr (\(Dependency item prereqs) ->  (flip (foldr insertWithNewID)) (item : prereqs)) Map.empty
-    where insertWithNewID k m = if k `Map.member` m then m else Map.insert k (Map.size m) m
+leafIndices = foldr (\(Dependency item prereqs) -> (flip (foldr insertWithNewIndex)) (item : prereqs)) Map.empty
+    where insertWithNewIndex k m = if k `Map.member` m then m else Map.insert k (Map.size m) m
 
 dependencyGraph :: Ord k => Map k Int -> [Dependency k] -> Graph
 dependencyGraph m deps =
