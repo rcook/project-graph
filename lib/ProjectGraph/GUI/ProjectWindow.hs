be5e624459b3f562cd7ec5acb94e44a0aad237ce
Richard Cook <rcook@rcook.org>
2018-11-16 22:41:32 -0800

    Pan and zoom

--- lib/ProjectGraph/GUI/ProjectWindow.hs
+++ lib/ProjectGraph/GUI/ProjectWindow.hs
@@ -4,6 +4,7 @@ module ProjectGraph.GUI.ProjectWindow (display) where
 
 import           Control.Monad (unless, when)
 import           Control.Monad.Trans.Reader (runReaderT)
+import           Data.Char (chr)
 import           Data.GraphViz
                     ( DotGraph
                     , GraphvizCommand(..)
@@ -28,7 +29,7 @@ import           Graphics.Rendering.Cairo
 import           Graphics.Rendering.Cairo.Internal (Render(..))
 import           Graphics.Rendering.Cairo.Types (Cairo(..))
 import           GI.Cairo (Context(..))
-import           GI.Gdk (EventMask(..), withManagedPtr)
+import           GI.Gdk (EventMask(..), ModifierType(..), withManagedPtr)
 import           GI.Gtk
                     ( AttrOp(..)
                     , DrawingArea(..)
@@ -44,22 +45,40 @@ import           Graphics.XDot.Types (Object(..), Operation, Point, Rectangle)
 import           Graphics.XDot.Viewer (drawAll)
 import           ProjectGraph.App (appTitle, gtkMain, initApp)
 
+type Objects = ([(Object String, Operation)], Rectangle)
+
+data Zoom = Proportional | ZoomLevel Int
+
 data State = State
-    { objects :: ([(Object String, Operation)], Rectangle)
+    { objects :: Objects
     , bounds :: [(Object String, Rectangle)]
     , mouseCoords :: Point
     , hover :: Object String
+    , zoom :: Zoom
     }
 
 type Width = Int
 
 type Height = Int
 
+initialState :: Objects -> State
+initialState objs = State objs [] (0, 0) None Proportional
+
 leftButton :: Word32
 leftButton = 1
 
 toXDotGraph :: (Ord a, ParseDotRepr dg n, PrintDot a) => DotGraph a -> IO (dg n)
 toXDotGraph dg = graphvizWithHandle Dot dg (XDot Nothing) hGetDot
+
+incrementZoom :: Zoom -> Zoom
+incrementZoom Proportional = ZoomLevel 10
+incrementZoom (ZoomLevel n) = ZoomLevel (n + 1)
+
+decrementZoom :: Zoom -> Zoom
+decrementZoom Proportional = ZoomLevel 10
+decrementZoom (ZoomLevel n)
+    | n == 1 = ZoomLevel 1
+    | otherwise = ZoomLevel (n - 1)
 
 display :: (Ord a, PrintDot a) => DotGraph a -> IO ()
 display dg = do
@@ -68,7 +87,7 @@ display dg = do
 
     let objs = (getOperations xdg, getSize xdg)
 
-    state <- newIORef $ State objs [] (0, 0) None
+    state <- newIORef $ initialState objs
 
     initApp
 
@@ -77,12 +96,24 @@ display dg = do
                 , #defaultWidth := 512
                 , #title := Text.pack appTitle
                 ]
-    #setPosition window WindowPositionCenter
-
-    on window #destroy mainQuit
 
     canvas <- new DrawingArea []
     #add window canvas
+
+    #setPosition window WindowPositionCenter
+
+    on window #keyPressEvent $ \eventKey -> do
+        keyState <- get eventKey #state
+        c <- (chr . fromIntegral) <$> get eventKey #keyval
+        when (keyState == [ ModifierTypeControlMask ]) $
+            case c of
+                '0' -> modifyIORef state (\s -> s { zoom = Proportional })
+                '-' -> modifyIORef state (\s -> s { zoom = decrementZoom (zoom s) })
+                '=' -> modifyIORef state (\s -> s { zoom = incrementZoom (zoom s) })
+        #queueDraw canvas
+        return True
+
+    on window #destroy mainQuit
 
     #addEvents canvas
                 [ EventMaskButtonPressMask
@@ -145,17 +176,25 @@ renderWithContext :: GI.Cairo.Context -> Render () -> IO ()
 renderWithContext ct r = withManagedPtr ct $ \p ->
     runReaderT (runRender r) (Cairo (castPtr p))
 
-redraw :: Width -> Height -> IORef State -> Render ()
-redraw rw rh state = do
-    s <- liftIO $ readIORef state
-
-    let (ops, size'@(_,_,sw,sh)) = objects s
-
-    -- Proportional scaling
+scaleDiagram :: Zoom -> Width -> Height -> Double -> Double -> (Double, Double, Double, Double)
+scaleDiagram Proportional rw rh sw sh =
     let scaleX = min (fromIntegral rw / sw) (fromIntegral rh / sh)
         scaleY = scaleX
         offsetX = 0.5 * fromIntegral rw
         offsetY = 0.5 * fromIntegral rh
+    in (scaleX, scaleY, offsetX, offsetY)
+scaleDiagram (ZoomLevel n) rw rh sw sh =
+    let offsetX = 0.5 * fromIntegral rw
+        offsetY = 0.5 * fromIntegral rh
+        s = fromIntegral n / 10.0
+    in (s, s, offsetX, offsetY)
+
+redraw :: Width -> Height -> IORef State -> Render ()
+redraw rw rh state = do
+    s <- liftIO $ readIORef state
+
+    let (ops, size'@(_, _, sw, sh)) = objects s
+        (scaleX, scaleY, offsetX, offsetY) = scaleDiagram (zoom s) rw rh sw sh
 
     save
     translate offsetX offsetY
